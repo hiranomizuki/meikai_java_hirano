fd50967db820b99de36bea49530edf5bbf1e6ad8
LAPTOP-FKRL8JAP\mihrn <mi.hrnmzk@gmail.com>
2022-11-16 20:08:44 +0900

    演習1-1作成完了

---
+++ enshu1_1/src/enshu1_1/Enshu1_1.java
@@ -0,0 +1,15 @@
+package enshu1_1;
+
+public class Enshu1_1 {
+
+	public static void main(String[] args) {
+		System.out.println("初めてのJavaプログラム。");
+		System.out.println("画面に出力しています");
+		
+		//セミコロンがないと、処理が終了しているとプログラム側が認知できない。→エラー
+		//セミコロンが欠如しているとエラーが出力される ↓出力エラー
+		// Exception in thread "main" java.lang.Error:Unresolved compilation problem:
+
+	}
+
+}
