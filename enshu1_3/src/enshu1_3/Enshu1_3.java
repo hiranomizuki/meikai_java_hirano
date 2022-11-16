57d72fc22ac6cbccd9b2ff56b88fc23189a345e2
LAPTOP-FKRL8JAP\mihrn <mi.hrnmzk@gmail.com>
2022-11-16 20:40:50 +0900

    演習1_3作成完了

---
+++ enshu1_3/src/enshu1_3/Enshu1_3.java
@@ -0,0 +1,13 @@
+package enshu1_3;
+
+public class Enshu1_3 {
+
+	public static void main(String[] args) {
+		
+		// printで出力
+		//\nで改行。今回は姓と名の間にも改行を要求されているため、下記の通り。
+		System.out.print("平\n野\n\n水\n稀");
+
+	}
+
+}
