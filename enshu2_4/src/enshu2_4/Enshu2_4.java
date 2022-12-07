440b9c6dc64c45499ec05f6da606fe29dafe56f6
LAPTOP-FKRL8JAP\mihrn <mi.hrnmzk@gmail.com>
2022-12-07 21:28:36 +0900

    コメントを追加

--- enshu2_4/src/enshu2_4/Enshu2_4.java
+++ enshu2_4/src/enshu2_4/Enshu2_4.java
@@ -7,11 +7,15 @@ public class Enshu2_4 {
 
 	public static void main(String[] args) {
 		
+		//Scannerインスタンス生成
 		Scanner stdIn = new Scanner(System.in);
 		
+		//整数値の入力欄を出力
 		System.out.print("整数値：");
+		//キーボードから入力された値をxに格納
 		int x = stdIn.nextInt();
 		
+		//xに10を加減した値をそれぞれ出力
 		System.out.println("10を加えた値は"+(x+10)+"です。");
 		System.out.println("10を減じた値は"+(x-10)+"です。");
 	}
