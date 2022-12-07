bed8b583651befe1a60340c5c50810a35bf64a3d
LAPTOP-FKRL8JAP\mihrn <mi.hrnmzk@gmail.com>
2022-12-07 21:35:25 +0900

    コメント追加

--- enshu2_5/src/enshu2_5/Enshu2_5.java
+++ enshu2_5/src/enshu2_5/Enshu2_5.java
@@ -6,14 +6,20 @@ import java.util.Scanner;
 public class Enshu2_5 {
 
 	public static void main(String[] args) {
+		//Scannerインスタンス生成
 		Scanner stdIn = new Scanner(System.in);
-		
+	
+		//xの値の入力を促す文字を出力
 		System.out.print("xの値：");
+		//xに実数値を入力してもらう
 		double x = stdIn.nextDouble();
 		
+		//yの値の入力を促す文字を出力
 		System.out.print("yの値：");
+		//yに実数値を入力してもらう
 		double y = stdIn.nextDouble();
 		
+		//xとyの値の合計値、平均値を計算しそれぞれ出力
 		System.out.println("合計は"+(x+y)+"です。");
 		System.out.println("平均は"+(x+y)/2+"です。");
 		
