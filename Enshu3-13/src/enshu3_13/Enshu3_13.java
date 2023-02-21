b8e72d7c470d040b5d120ac8a1919570f4b7037c
LAPTOP-FKRL8JAP\mihrn <mi.hrnmzk@gmail.com>
2023-02-21 20:12:24 +0900

    自分の元の回答にif分の案追記メモ

--- Enshu3-13/src/enshu3_13/Enshu3_13.java
+++ Enshu3-13/src/enshu3_13/Enshu3_13.java
@@ -7,38 +7,78 @@ public class Enshu3_13 {
 
 	public static void main(String[] args) {
 		Scanner stdIn = new Scanner(System.in);
-		
+
 		System.out.println("整数A：");
 		int a = stdIn.nextInt();
-		
+
 		System.out.println("整数B：");
 		int b = stdIn.nextInt();
 
 		System.out.println("整数C：");
 		int c = stdIn.nextInt();
-		
-		
+
+
 		int min = a;
 		if (b<min)
 			min=b;
 		if (c<min)
 			min=c;
-		
+
 		int max = a;
 		if (b>max)
 			max=b;
 		if (c>max)
 			max=c;
-		
+
 		int mid = a;
 		if(min!=b && max!=b)
 			mid =b;
 		if(min!=c && max !=c)
 			mid=c;
-		
+
 		System.out.println("中央値："+mid);
-		
-		
+
+
 	}
 
-}
+}		
+		
+		
+//		if文で分岐させる方法も（未完成^^）
+//
+//		int mid=a;
+//		
+//		if(a<b) {
+//			if(b<c) {
+//				mid =b;
+//			}
+//			else {
+//				//a<b b>c
+//				if(a<c) {
+//					//a<b c<b a<c
+//					mid=c;
+//				}
+//				else mid=a;
+//			}
+//			//a>bの場合
+//			if(a<c) {
+//				mid=a;
+//			}
+//			else {
+//				//a>b a>c
+//				if(b<c)
+//					mid=c;
+//				else mid=b;
+//			}
+//			
+//		} 
+//			
+//		
+//		
+//		
+//		System.out.println("中央値："+mid);
+		
+		
+	
+
+
