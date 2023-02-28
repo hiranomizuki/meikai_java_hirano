23c78cc734798af2f3b3ac165d0761aa8f0121a8
LAPTOP-FKRL8JAP\mihrn <mi.hrnmzk@gmail.com>
2023-02-28 20:39:01 +0900

    前半の別回答ver.

---
+++ Enshu3-16/src/Enshu3_16/Answer2.java
@@ -0,0 +1,37 @@
+
+package Enshu3_16;
+import java.util.Scanner;
+public class Answer2 {
+
+	public static void main(String[] args) {
+		
+		//3-16の前半Answer2
+	Scanner stdIn = new Scanner(System.in);
+		
+	int i;
+	int a=0;
+		
+		for(i=1 ; i<=3 ; i++) {
+
+			a=0;
+			
+			System.out.println("整数"+i+":");
+			a = stdIn.nextInt();
+			
+			if (a == 0) {
+				System.out.println("整数"+i+"には0以外の整数を入力してください。");
+				i--;
+			}
+			
+		}
+		
+		if(i == 4 && a !=0) {
+			System.out.println("またやってみてね！");
+			
+			
+		}	
+		stdIn.close();
+	}
+
+}
+
