8cfeda1904516a3dc59b05144fa7d97377528a55
LAPTOP-FKRL8JAP\mihrn <mi.hrnmzk@gmail.com>
2023-04-13 21:28:09 +0900

    大平さんアドバイスを反映、インデント含む

--- Enshu4_8/src/Enshu4_8/Enshu4_8.java
+++ Enshu4_8/src/Enshu4_8/Enshu4_8.java
@@ -3,6 +3,7 @@ package Enshu4_8;
 import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+
 public class Enshu4_8 {
 
 	public static void main(String[] args) {
@@ -11,33 +12,40 @@ public class Enshu4_8 {
 		Scanner stdIn = new Scanner(System.in);
 		
 		int imputnumber;
-			try {
-				imputnumber = stdIn.nextInt();	
-			} 
-			catch(InputMismatchException e) {
-				System.err.print("値が不正です。範囲および型を確認してください。");
-				return;
-			} 
-			catch(NoSuchElementException e){
-				System.err.print("値が不正です。範囲および型を確認してください。");
-				return;
-			} 
-			catch(IllegalStateException e){
-				System.err.print("不正または不適切なときにメソッドが呼び出されています。");
-				return;
-				} 
-			finally {
-				stdIn.close();
-			}
-			
-			int characterlength = 0;
-			
-		while(imputnumber % 10 >= 1) {
-			characterlength += 1;
-			imputnumber /= 10;
+		
+		try {
+			imputnumber = stdIn.nextInt();	
+		} 
+		//nextIntおせば、Exceptionの候補でてくる
+		catch(InputMismatchException e) {
+			System.err.print("値が不正です。範囲および型を確認してください。");
+			return;
+		} 
+		catch(NoSuchElementException e) {
+			System.err.print("値が不正です。範囲および型を確認してください。");
+			return;
+		} 
+		catch(IllegalStateException e) {
+			System.err.print("不正または不適切なときにメソッドが呼び出されています。");
+			return;
+		} 
+		finally {
+			stdIn.close();
 		}
 		
-		System.out.println("その値は"+characterlength+"桁です。");
+		int characterlength = 0;
+		
+		//10→定数final 変更しやすいように。 
+		final int TEN=10;
+		
+		//入力値を10でわった値が1以上のときは1桁以上の値のはずなので、この条件式
+		//1254/10=125.4 → 125.4/10=12.54 → 12.54/10=1.254 
+		while(imputnumber / TEN >= 1) {
+			characterlength ++;
+			imputnumber /= TEN;
+		}
+		
+		System.out.println("その値は" + characterlength + "桁です。");
 		
 		
 		
