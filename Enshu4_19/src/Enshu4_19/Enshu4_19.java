e6dcd7e02e5345738f5600ecb736514408d9650b
LAPTOP-FKRL8JAP\mihrn <mi.hrnmzk@gmail.com>
2023-06-27 21:27:29 +0900

    インデント等を調整

--- Enshu4_19/src/Enshu4_19/Enshu4_19.java
+++ Enshu4_19/src/Enshu4_19/Enshu4_19.java
@@ -12,23 +12,24 @@ public class Enshu4_19 {
 		System.out.println("季節を求めます。何月ですか？");
 		
 		do {
-		month = stdIn.nextInt();
-		
-		if ( 3 <= month && month <= 5) {
-			System.out.println("それは春です");
-		}
-		else if (6 <= month && month <= 8) {
-			System.out.println("それは夏です");
-		}
-		else if( 9 <= month && month <=11) {
-			System.out.println("それは秋です");
-		}
-		else if(month == 12 || month ==1 || month ==2) {
-			System.out.println("それは冬です");
-		}else {
-			System.out.println("もう一度入力してください。");
-		}
-		}while(month < 1 || month >12);
+			month = stdIn.nextInt();
+			
+			if ( 3 <= month && month <= 5) {
+				System.out.println("それは春です");
+			}
+			else if ( 6 <= month && month <= 8 ) {
+				System.out.println("それは夏です");
+			}
+			else if ( 9 <= month && month <=11) {
+				System.out.println("それは秋です");
+			}
+			else if (month == 12 || month == 1 || month == 2 ) {
+				System.out.println("それは冬です");
+			}
+			else {
+				System.out.println("もう一度入力してください。");
+			}
+		} while(month < 1 || month >12);
 
 	}
 }
