6548113921c6ed96a333ba0b431a8d0878382ee9
LAPTOP-FKRL8JAP\mihrn <mi.hrnmzk@gmail.com>
2023-07-10 22:03:52 +0900

    とき宣自己紹介（配列のお勉強）

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -3,8 +3,24 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello World");
-	}
 
+		String groupName="超ときめき♡宣伝部";
+		
+		String[] memberName = new String[6];
+		memberName[0]="辻野かなみ";
+		memberName[1]="杏ジュリア";
+		memberName[2]="坂井仁香";
+		memberName[3]="小泉遥香";
+		memberName[4]="菅田愛貴";
+		memberName[5]="吉川ひより";
+		
+		for(int i = 0; i<memberName.length; i++) {
+		System.out.println(memberName[i]+"です！");
+		}
+//全員の自己紹介が終わったら下記グループの挨拶をして終了
+		
+		System.out.println("私たち"+groupName+"です！");
+		
+		}
+	
 }
