8fe21026d1b3402279a69ce54c6c32211c291ba4
LAPTOP-FKRL8JAP\mihrn <mi.hrnmzk@gmail.com>
2023-07-11 21:47:03 +0900

    変数を共通化・・平野の気づきから具体策を大平さんからご教授いただいた

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -1,26 +1,53 @@
 package test;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
-
-		String groupName="超ときめき♡宣伝部";
 		
-		String[] memberName = new String[6];
-		memberName[0]="辻野かなみ";
-		memberName[1]="杏ジュリア";
-		memberName[2]="坂井仁香";
-		memberName[3]="小泉遥香";
-		memberName[4]="菅田愛貴";
-		memberName[5]="吉川ひより";
+		Scanner stdIn = new Scanner(System.in);
+		System.out.println("オリジナルの名前を表示するなら1、今の名前を表示するなら2を入力してください。");
+		int number = stdIn.nextInt();
 		
-		for(int i = 0; i<memberName.length; i++) {
-		System.out.println(memberName[i]+"です！");
+		String		groupName = null;
+		String[]	memberNames = new String[6];
+		
+		
+		if ( number == 1) {
+			
+			String groupName1="ときめき♡宣伝部";
+			memberNames[0]="辻野かなみ";
+			memberNames[1]="藤本ばんび";
+			memberNames[2]="坂井仁香";
+			memberNames[3]="小泉遥香";
+			memberNames[4]="小高サラ";
+			memberNames[5]="吉川ひより";	
+			
+			//この部分をメソッド化して、ここでそのメソッドを呼び出すようにすれば、共通処理になる
+			//memberNamesも外部ファイルにだすようにチャレンジしてみよう！
+			
+			for(int i = 0; i<memberNames.length; i++) {
+			
+				System.out.println(memberNames[i]+"です！");
+			}
+			//全員の自己紹介が終わったら下記グループの挨拶をして終了
+			System.out.println("私たち"+groupName1+"です！");
+			
+		}else if( number == 2) {
+			
+			String groupName1="超ときめき♡宣伝部";
+			memberNames[0]="辻野かなみ";
+			memberNames[1]="杏ジュリア";
+			memberNames[2]="坂井仁香";
+			memberNames[3]="小泉遥香";
+			memberNames[4]="菅田愛貴";
+			memberNames[5]="吉川ひより";
+			for(int i = 0; i<memberNames.length; i++) {
+				System.out.println(memberNames[i]+"です！");
+			}
+			//全員の自己紹介が終わったら下記グループの挨拶をして終了
+			System.out.println("私たち"+groupName1+"です！！");
 		}
-//全員の自己紹介が終わったら下記グループの挨拶をして終了
-		
-		System.out.println("私たち"+groupName+"です！");
-		
-		}
-	
+	}
 }
